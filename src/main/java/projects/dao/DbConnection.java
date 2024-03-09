3b406a74755d0cc00cd76a0be2b94b49c7571310
Tcarrillo06 <157557154+Tcarrillo06@users.noreply.github.com>
2024-03-08 18:45:58 -0800

    Added Project Dao

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -7,16 +7,16 @@ import java.sql.SQLException;
 import projects.exception.DbException;
 
 public class DbConnection {
-	
+
 	private static String HOST = "localhost";
 	private static String PASSWORD = "projects";
 	private static int PORT = 3306;
 	private static String SCHEMA = "projects";
 	private static String USER = "projects";
-	
+
 	public static Connection getConnection() {
 		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s", HOST, PORT, SCHEMA, USER, PASSWORD);
-		
+
 		try {
 			Connection conn = DriverManager.getConnection(uri);
 			System.out.println("Connection to schema '" + SCHEMA + "' is successful.");
@@ -24,7 +24,7 @@ public class DbConnection {
 		} catch (SQLException e) {
 			System.out.println("Unable to get connection at " + uri);
 			throw new DbException("Unable to get connection at \" + uri");
-			
-		}	
-	}	
+
+		}
+	}
 }
